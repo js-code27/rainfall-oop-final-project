abca16d1f75c238bd9003e1ed942fba8529ccf0a
js-code27 <santosjorge717@gmail.com>
2026-05-18 23:37:34 -0400

    A lot of things

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,48 @@ public class Main
 {
     public static void main(String[] args)
     {
-
+        // Creates the Scanner object to read input
         Scanner input = new Scanner(System.in);
 
+        // Stores the users menu option
         int option;
 
+        // Stores the users selected year
         int selectedYear
+
+        double[][] rainfallData = {{4.96, 3.27, 1.50, 1.38, 2.28, 2.11, 1.61, 5.93, 4.30, 1.77, 8.72, 3.47},
+                {1.60, 3.88, 2.22, 10.02, 7.16, 2.74, 5.10, 5.14, 4.91, 6.39, 17.65, 4.30},
+                {1.81, 1.70, 6.04, 6.25, 4.53, 3.93, 6.66, 7.68, 15.79, 6.61, 10.16, 3.33}}
+
+        // Creats rainfall object with preset data
+        Rainfall rainfall = new Rainfall(rainfallData);
+
+        System.out.println("Rainfall Measurement Program");
 
         do
         {
+            switch (option)
+            {
+                case A:
 
-        }
+                    break;
+
+                case B:
+
+                    break;
+
+                case C:
+
+                    break;
+
+                case D:
+
+                    break;
+
+
+            }
+
+        } while (option != 9);
 
     }
 
@@ -31,13 +62,14 @@ public class Main
         System.out.println("\nNOTE: Rain is measured in inches");
     }
 
+    // Acquires and validates the user number option for year selection
     public static int askForYear(Scanner keyboard)
     {
         int year;
 
         do
         {
-            System.out.print("Select a number that corresponds to the year you want: ");
+            System.out.print("Select from 1-3 for a specific year: ");
             year = keyboard.nextInt();
 
             if (year < 1 || year > 3)
@@ -45,7 +77,8 @@ public class Main
                 System.out.println("Please select year 1, 2, or 3");
             }
 
-        } while (year < 1 || year > 3)
+        } while (year < 1 || year > 3);
 
+        return year;
     }
 }
--- src/Rainfall.java
+++ src/Rainfall.java
@@ -1,7 +1,20 @@
 public class Rainfall
 {
+    // Stores 3 years of rainfall data
     private double[][] rainfall;
-    private String[] months;
+
+    // Stores the months
+    private String[] months = {"January", "February", "March", "April",  "May", "June", "July", "August",  "September", "October", "November", "December"};
+
+    public Rainfall()
+    {
+        rainfall = new double[3][12];
+    }
+
+    public Rainfall(double[][] rainfallData)
+    {
+
+    }
 
 
 
